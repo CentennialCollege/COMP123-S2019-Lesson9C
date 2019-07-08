2424222901f50977d089888ba64bb610ac55fb0d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-07-08 16:27:05 -0400

    added basic code to convert input to integer values

--- COMP123-S2019-Lesson9C/CalculatorForm.cs
+++ COMP123-S2019-Lesson9C/CalculatorForm.cs
@@ -12,9 +12,36 @@ namespace COMP123_S2019_Lesson9C
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// This is the contructor for the CalculatorForm
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is a shared event handler for the CalculatorButton click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            var TheButton = sender as Button;
+
+            int ButtonValue;
+            bool Result = int.TryParse(TheButton.Text, out ButtonValue);
+
+            if(Result)
+            {
+                ResultLabel.Text = TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "Not a Number (NAN)";
+            }
+
+            
         }
     }
 }
